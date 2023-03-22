12a57cd601660e42a575332ed2ba6004fe0accff
NeelimaIT <127119061+NeelimaIT@users.noreply.github.com>
2023-03-22 22:15:54 +0600

    Add files via upload

---
+++ index1.php
@@ -0,0 +1,16 @@
+<html>
+    
+
+<body>
+<form action="./form.php" method="GET" class="col-4 mx-auto mt-5">
+    <div class="mb-3">
+        <label for="exampleInputemail" class="form-label">NUMBER</label>
+        <input name="number" type="text" class="form-control" id="exampleInputemail">
+</div>
+<button type="submit" class= "btn btn-primary">Submit</button>
+</form>
+
+
+
+</body>
+</html>
